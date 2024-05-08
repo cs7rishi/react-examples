ec7f7b934cf571b6d192395c4be1e7d91cc12d66
Rishi Singh <cs24rishi@gmail.com>
2024-05-09 00:40:41 +0530

    removed old files

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -1,15 +1,8 @@
-import { Header } from "./components/Header"
-import { Footer } from "./components/Footer"
 import "./App.css"
 
 export default function App(){
-    let name = "Rishi"
     return (
         <div className='App'>
-            <Header/>
-            <h1>{name}</h1>
-            <h2>Heading 2</h2>
-            <Footer/>
         </div>
     )
 }
