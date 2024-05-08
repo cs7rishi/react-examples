12c61922244ae1cd70bb6c76356c8cc060acf01c
Rishi Singh <cs24rishi@gmail.com>
2024-05-09 00:34:26 +0530

    explored css and different components

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import { Header } from "./components/Header"
+import { Footer } from "./components/Footer"
+import "./App.css"
 
 export default function App(){
     let name = "Rishi"
     return (
         <div className='App'>
+            <Header/>
             <h1>{name}</h1>
             <h2>Heading 2</h2>
+            <Footer/>
         </div>
     )
 }
---
+++ taskmate/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Logo from "../assets/logo.png"
+import "./Header.css"
+
+export const Header = () => {
+  return (
+    <div>
+        <img src={Logo} className="logo" alt = ""/>
+        Header</div>
+  )
+}
