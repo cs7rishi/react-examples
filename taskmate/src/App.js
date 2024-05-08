2b4cb654f1789c6a500bd6df298125f4197ff4cd
Rishi Singh <cs24rishi@gmail.com>
2024-05-09 01:05:44 +0530

    explored useStates

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -1,8 +1,32 @@
+import { useState } from "react";
 import "./App.css"
 
 export default function App(){
+    let [count, setCount] = useState(0);
+
+    function handleAdd(){
+        setCount(count => count + 1);
+        setCount(count => count + 1);
+        setCount(count => count + 1);
+    }
+
+    function handleSub(){
+        setCount(count-1);
+    }
+
+    function handleReset(){
+        setCount(0);
+    }
+
     return (
         <div className='App'>
+            <div className="box">
+                <p>{count}</p>
+                <button className="add" onClick={handleAdd}>ADD</button>
+                {/* <button className="add" onClick={() => count+=1}>ADD</button> */}
+                <button className="sub" onClick={handleSub}>SUB</button>
+                <button className="reset" onClick={handleReset}>RESET</button>
+            </div>
         </div>
     )
 }
