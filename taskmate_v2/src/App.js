d9e7165694274242413db3bdbe200e8ab2f8135f
Rishi Singh <cs24rishi@gmail.com>
2024-05-16 16:11:48 +0530

    explored local storage

--- taskmate_v2/src/App.js
+++ taskmate_v2/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import { AddTask } from './components/AddTask';
 import { Header } from './components/Header';
 import { ShowTask } from './components/ShowTask';
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 function App() {
-  const [tasklist, setTasklist] = useState([]);
+  const [tasklist, setTasklist] = useState( JSON.parse(localStorage.getItem("tasklist")) || []);
   const [task, setTask] = useState({})
+
+  useEffect(()=>{
+    localStorage.setItem("tasklist", JSON.stringify(tasklist))
+  }, [tasklist]);
 
   return (
     <>
