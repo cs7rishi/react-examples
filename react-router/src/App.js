c951068e687bbdd379e9ffb24d0fb88828a55ae7
Rishi Singh <cs24rishi@gmail.com>
2024-05-18 02:07:56 +0530

    explored navigation

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,24 +1,29 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import './App.css';
-import {Header} from "./components/Header";
-import {Footer} from "./components/Footer"
+import { Admin } from "./components/Admin"
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer"
 import { ProductList } from "./components/ProductList";
 import { Home } from "./components/Home";
 import { ProductDetail } from "./components/ProductDetail";
 import { Contact } from "./components/Contact";
 
 function App() {
+
+  const admin = true;
+
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/products" element={<ProductList />}></Route>
         <Route path="/products/1001" element={<ProductDetail />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/admin" element={admin ? <Admin/> : <Navigate to="/" />} />
         <Route></Route>
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- react-router/src/components/Contact.js
+++ react-router/src/components/Contact.js
@@ -1,5 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
 export const Contact = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = ()=>{
+    console.log("redirecting user")
+    return navigate("/")
+  }
   return (
+    <>
     <div className="component">Contact</div>
+    <button onClick={handleSubmit}>Go To Home</button>
+    </>
   )
 }
